465e3d2e85263ec3d90b5263266fb7a7c22d59df
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-05-13 18:28:23 +0200

    loading changed

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ const Loading = () => {
     <div className='loading-container'>
       <img
         id='loading'
-        src='https://media0.giphy.com/media/BwxFdYKFvhGQ7CEY03/giphy.gif?cid=6c09b952cvuf3qvjagx6bq78k53yhjhqjkax251db1hiv2cg&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=s'
+        src='https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXE1N3FldGM1dG9pODVweDY5cm1uM2Y0ZmQyc2I3b2t6aWU5MnVyOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/YDC5HjHcfFk8lgxXQm/giphy.gif'
         alt='loading'
       />
     </div>
