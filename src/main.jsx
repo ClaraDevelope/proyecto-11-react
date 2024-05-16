501e832b18c95dc3149aebbbfe3c6b9af391344e
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-05-16 09:55:40 +0200

    guardado 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Characters from './pages/Characters/Characters'
-import Comics from './pages/Comics/Comics'
-import Details from './components/Details/Details'
-import NotFound from './pages/404/NotFound' // Importa tu componente NotFound
+import Home from './pages/Home/Home.jsx'
+import Comics from './pages/Comics/Comics.jsx'
+import NotFound from './pages/404/NotFound.jsx'
+import Details from './components/Details/Details.jsx'
+import Characters from './pages/Characters/Characters.jsx'
 
-const App = () => {
-  return (
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename='/'>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/characters' element={<Characters />} />
-        <Route
-          path='/characters/:id'
-          element={<Details entityType='characters' />}
-        />
-        <Route path='/comics' element={<Comics />} />
-        <Route path='/comics/:id' element={<Details entityType='comics' />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path='/' element={<App />}>
+          <Route index element={<Home />} />
+          <Route path='/characters' element={<Characters />} />
+          <Route
+            path='/characters/:id'
+            element={<Details entityType='characters' />}
+          />
+          <Route path='/comics' element={<Comics />} />
+          <Route path='/comics/:id' element={<Details entityType='comics' />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-}
-
-export default App
+  </React.StrictMode>
+)
