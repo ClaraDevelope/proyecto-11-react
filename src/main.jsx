b146f696efe5f43c3a2e178693935877c93a25c6
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-05-16 09:53:32 +0200

    guardado

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home.jsx'
-import Comics from './pages/Comics/Comics.jsx'
-import NotFound from './pages/404/NotFound.jsx'
-import Details from './components/Details/Details.jsx'
-import Characters from './pages/Characters/Characters.jsx'
+import Home from './pages/Home/Home'
+import Characters from './pages/Characters/Characters'
+import Comics from './pages/Comics/Comics'
+import Details from './components/Details/Details'
+import NotFound from './pages/404/NotFound' // Importa tu componente NotFound
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename='/'>
+const App = () => {
+  return (
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<App />}>
-          <Route index element={<Home />} />
-          <Route path='/characters' element={<Characters />} />
-          <Route
-            path='/characters/:id'
-            element={<Details entityType='characters' />}
-          />
-          <Route path='/comics' element={<Comics />} />
-          <Route path='/comics/:id' element={<Details entityType='comics' />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
+        <Route path='/' element={<Home />} />
+        <Route path='/characters' element={<Characters />} />
+        <Route
+          path='/characters/:id'
+          element={<Details entityType='characters' />}
+        />
+        <Route path='/comics' element={<Comics />} />
+        <Route path='/comics/:id' element={<Details entityType='comics' />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  )
+}
+
+export default App
