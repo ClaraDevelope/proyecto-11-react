05d62c3b6fa6aaee1d943678fd4f608f4d43aa59
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-05-21 21:50:57 +0200

    hovers

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,9 +7,9 @@ const Card = ({ id, title, path, entityType }) => {
     <div className='card-container'>
       <Link to={`/${entityType}/${id}`} className='card-link'>
         <li className='card' id={id}>
-          <h3 className='card-title'>{title}</h3>
           <div className='image-container'>
             <img src={`${path}`} alt={title} loading='lazy' />
+            <h3 className='card-title'>{title}</h3>
           </div>
         </li>
       </Link>
@@ -25,28 +25,3 @@ Card.propTypes = {
 }
 
 export default Card
-
-// import { Link } from 'react-router-dom'
-// import PropTypes from 'prop-types'
-// import './Card.css'
-// const Card = ({ id, title, path }) => {
-//   return (
-//     <div className='card-container'>
-//       <Link to={`/characters/${id}`} className='card-link'>
-//         <li className='card' id={id}>
-//           <h3 className='card-title'>{title}</h3>
-//           <div className='image-container'>
-//             <img src={`${path}`} alt={title} loading='lazy' />
-//           </div>
-//         </li>
-//       </Link>
-//     </div>
-//   )
-// }
-
-// Card.propTypes = {
-//   id: PropTypes.number.isRequired,
-//   title: PropTypes.string.isRequired,
-//   path: PropTypes.string.isRequired
-// }
-// export default Card
